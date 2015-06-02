2648f0fb7d2b8bd87f927586c5fbd811294bcd88
Thomas Alcorn <tom@adharmonics.com>
2015-06-02 16:43:54 -0400

    Fixing dumb mistake, making force_control a kwarg

--- lib/split/helper.rb
+++ lib/split/helper.rb
@@ -2,12 +2,13 @@ module Split
   module Helper
     module_function
 
-    def ab_test(metric_descriptor, control = nil, *alternatives, force_control = false)
+    def ab_test(metric_descriptor, control = nil, *alternatives, **kwargs)
       begin
         experiment = ExperimentCatalog.find_or_initialize(metric_descriptor, control, *alternatives)
 
         alternative = if Split.configuration.enabled
           experiment.save
+          force_control = kwargs[:force_control] ||= false
           trial = Trial.new(:user => ab_user, :experiment => experiment,
               :override => override_alternative(experiment.name), :exclude => exclude_visitor?,
               :disabled => split_generically_disabled?)
